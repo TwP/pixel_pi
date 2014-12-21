ee121454599705f4515b0443a8dccd64bc524361
Tim Pease <tim.pease@gmail.com>
2014-12-21 14:07:36 -0700

    pull in the ws2811 files when compiling

--- ext/pixel_pi/extconf.rb
+++ ext/pixel_pi/extconf.rb
@@ -1,3 +1,18 @@
 require 'mkmf'
 
+pixel_pi_path = File.expand_path("../", __FILE__)
+ws2811_path   = File.expand_path("../../ws2811", __FILE__)
+ws2811_files  = %w[
+  clk.h
+  dma.h
+  gpio.h
+  pwm.h
+  ws2811.h
+  leds.c
+  dma.c
+  ws2811.c
+]
+ws2811_files.map! { |name| "#{ws2811_path}/#{name}" }
+FileUtils.cp(ws2811_files, pixel_pi_path)
+
 create_makefile("neo_pixels/leds")
