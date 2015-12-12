f1463ac09146a6880625ad3662be9579d1d1e515
Tim Pease <tim.pease@gmail.com>
2015-12-12 11:59:41 -0700

    Non RaspberryPI installation
    Check the host architecture flag before trying to build the native extension.
    This trick allows us to install this gem on non-raspberrypi machines. This is
    the first step to supporting local development.

--- ext/pixel_pi/extconf.rb
+++ ext/pixel_pi/extconf.rb
@@ -1,18 +1,36 @@
 require 'mkmf'
+require 'rbconfig'
 
 pixel_pi_path = File.expand_path("../", __FILE__)
 ws2811_path   = File.expand_path("../../ws2811", __FILE__)
-ws2811_files  = %w[
-  clk.h
-  dma.h
-  gpio.h
-  pwm.h
-  ws2811.h
-  dma.c
-  pwm.c
-  ws2811.c
-]
-ws2811_files.map! { |name| "#{ws2811_path}/#{name}" }
-FileUtils.cp(ws2811_files, pixel_pi_path)
 
-create_makefile("pixel_pi/leds")
+if RbConfig::CONFIG["arch"].to_s =~ /\Aarm-linux/i
+  ws2811_files  = %w[
+    clk.h
+    dma.h
+    gpio.h
+    pwm.h
+    ws2811.h
+    dma.c
+    pwm.c
+    ws2811.c
+  ]
+  ws2811_files.map! { |name| "#{ws2811_path}/#{name}" }
+  FileUtils.cp(ws2811_files, pixel_pi_path)
+
+  create_makefile("pixel_pi/leds")
+else
+  File.open("#{pixel_pi_path}/Makefile", "w") do |fd|
+    fd.write <<-MAKEFILE
+all: ;
+install: ;
+static: ;
+install-so: ;
+install-rb: ;
+clean: ;
+clean-so: ;
+clean-static: ;
+clean-rb: ;
+    MAKEFILE
+  end
+end
